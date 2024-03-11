a7c24419ab2fd48915c3fc2954d7494d4f64ff5e
BaRemy <bahatiremy05@gmail.com>
2024-03-11 16:54:24 -0700

    Refactor: Remove unused variable

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 const argLen = process.argv.length - 2;
 console.log(argLen === 0 ? 'No argument' : argLen === 1 ? 'Argument found' : 'Arguments found');
